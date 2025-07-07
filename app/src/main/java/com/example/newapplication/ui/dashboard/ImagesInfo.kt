a3a12ef06c59f612b6c8094728dacab3c671c750
jaeho <jeeno816@kaist.ac.kr>
2025-07-07 15:20:26 +0900

    detailFragment만듦.

--- app/src/main/java/com/example/newapplication/ui/dashboard/ImagesInfo.kt
+++ app/src/main/java/com/example/newapplication/ui/dashboard/ImagesInfo.kt
@@ -1,3 +1,3 @@
 package com.example.newapplication.ui.dashboard
 
-data class ImagesInfo(val country:String,val user:String, val url:String)
+data class ImagesInfo(val country:String,val user:String, val url:String, val starbar:Int, val locationinfo:String)
